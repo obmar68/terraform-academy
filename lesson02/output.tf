022f80f5b9dee1bdf2f1138ae54a641479cb3970
obmar68 <miguel.oseguera@wizeline.com>
2020-01-29 13:09:53 -0600

    Uncommented Outputs

--- lesson02/output.tf
+++ lesson02/output.tf
@@ -1,7 +1,7 @@
 # un comment once you have solution
-# output "elb_dns" {
-#   value = "${aws_elb.elb.dns_name}"
-# }
-# output "web_dns" {
-#   value = "${aws_route53_record.dns_web.name}"
-# }
+ output "elb_dns" {
+   value = "${aws_elb.elb.dns_name}"
+ }
+ output "web_dns" {
+   value = "${aws_route53_record.dns_web.name}"
+ }
